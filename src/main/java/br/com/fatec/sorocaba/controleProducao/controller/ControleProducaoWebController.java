699dffd8cb7bd7f3d238b2c418bc750ac2570a89
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2021-10-20 23:35:43 -0300

    CriarRepositoryServiceUsuarios

--- src/main/java/br/com/fatec/sorocaba/controleProducao/controller/ControleProducaoWebController.java
+++ src/main/java/br/com/fatec/sorocaba/controleProducao/controller/ControleProducaoWebController.java
@@ -1,19 +1,54 @@
 package br.com.fatec.sorocaba.controleProducao.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.fatec.sorocaba.controleProducao.model.Usuario;
+import br.com.fatec.sorocaba.controleProducao.service.UsuarioService;
 
 @Controller
 public class ControleProducaoWebController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
 
-	@GetMapping("/usuarios")
-	public ModelAndView login() {
+	@GetMapping("/usuario")
+	public ModelAndView novo() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("usuario", new Usuario());
-		modelAndView.setViewName("usuarios/index");
+		modelAndView.setViewName("usuario/index");
 		return modelAndView;
 	}
+	
+	@PostMapping("/usuario")
+	public ModelAndView save(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+		ModelAndView mv = new ModelAndView("redirect:/usuario/list");
+		
+		try {
+			usuarioService.save(usuario);
+		} catch (Exception e) {
+			mv.addObject("mensagemErro", "Não foi possível salvar ou editar esse usuário!");
+		}
+		attributes.addFlashAttribute("mensagemSucesso", "Usuário criado ou alterado com sucesso!");
+		return mv;
+	}
+	
+	@GetMapping("usuario/list")
+	public ModelAndView list() {
+		List<Usuario> usuarios = usuarioService.list();
+		ModelAndView mv = new ModelAndView("usuario/list");
+		mv.addObject("usuarios", usuarios);
+		return mv;
+	}
+	
+	
 }
---
+++ src/main/java/br/com/fatec/sorocaba/controleProducao/service/UsuarioService.java
@@ -0,0 +1,28 @@
+package br.com.fatec.sorocaba.controleProducao.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fatec.sorocaba.controleProducao.model.Usuario;
+import br.com.fatec.sorocaba.controleProducao.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Transactional
+	public Usuario save(Usuario usuario) {
+		return usuarioRepository.saveAndFlush(usuario);
+	}
+
+	public List<Usuario> list() {
+		return usuarioRepository.findAll();
+	}
+	
+}
